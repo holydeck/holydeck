d4aaf6b7274eabc67d72958584b7867e692820e9
DRiiFT KiinG <git@holydeck.faith>
2026-09-08 00:49:30 +0200

    feat(core): sermon file parsing with legacy format support

--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/messages.ts', 'src/references.ts', 'src/translations.ts', 'src/canon.ts', 'src/config.ts'],
+  entry: ['src/messages.ts', 'src/references.ts', 'src/translations.ts', 'src/canon.ts', 'src/config.ts', 'src/sermon.ts'],
   format: ['esm'],
   dts: true,
   clean: true,
